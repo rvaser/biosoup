57fed5f1d087e6e20b77e85a37de23182dfc9582
rvaser <robert.vaser@gmail.com>
2020-03-25 12:19:58 +0100

    Move strand to lhs_id; Add score to Overlap

--- include/biosoup/overlap.hpp
+++ include/biosoup/overlap.hpp
@@ -14,25 +14,25 @@ struct Overlap {
 
   Overlap(std::uint64_t lhs_id, std::uint32_t lhs_begin, std::uint32_t lhs_end,
           std::uint64_t rhs_id, std::uint32_t rhs_begin, std::uint32_t rhs_end,
-          std::uint32_t strand)
+          std::uint32_t score)
       : lhs_id(lhs_id),
         lhs_begin(lhs_begin),
         lhs_end(lhs_end),
         rhs_id(rhs_id),
         rhs_begin(rhs_begin),
         rhs_end(rhs_end),
-        strand(strand),
+        score(score),
         alignment() {}
 
   Overlap(std::uint64_t lhs_id, std::uint32_t lhs_begin, std::uint32_t lhs_end,
           std::uint64_t rhs_id, std::uint32_t rhs_begin, std::uint32_t rhs_end,
-          std::uint32_t strand, const std::string& alignment)
-      : Overlap(lhs_id, lhs_begin, lhs_end, rhs_id, rhs_begin, rhs_end, strand,
+          std::uint32_t score, const std::string& alignment)
+      : Overlap(lhs_id, lhs_begin, lhs_end, rhs_id, rhs_begin, rhs_end, score,
                 alignment.c_str(), alignment.size()) {}
 
   Overlap(std::uint64_t lhs_id, std::uint32_t lhs_begin, std::uint32_t lhs_end,
           std::uint64_t rhs_id, std::uint32_t rhs_begin, std::uint32_t rhs_end,
-          std::uint32_t strand, const char* alignment,
+          std::uint32_t score, const char* alignment,
           std::uint32_t alignment_length)
       : lhs_id(lhs_id),
         lhs_begin(lhs_begin),
@@ -40,7 +40,7 @@ struct Overlap {
         rhs_id(rhs_id),
         rhs_begin(rhs_begin),
         rhs_end(rhs_end),
-        strand(strand),
+        score(score),
         alignment(alignment, alignment_length) {}
 
   Overlap(const Overlap&) = default;
@@ -51,15 +51,13 @@ struct Overlap {
 
   ~Overlap() = default;
 
-  std::uint64_t lhs_id;
+  std::uint64_t lhs_id;  // (optional) LSB used for Watson-Crick strand
   std::uint32_t lhs_begin;
   std::uint32_t lhs_end;
-
   std::uint64_t rhs_id;
   std::uint32_t rhs_begin;
   std::uint32_t rhs_end;
-
-  std::uint32_t strand;
+  std::uint32_t score;  // heuristic or exact alignment score
   std::string alignment;  // (optional) cigar string
 };
 
