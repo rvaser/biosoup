adf09c0002170134b45ae76809699410288c15a9
rvaser <robert.vaser@gmail.com>
2020-03-31 15:29:17 +0200

    Update formatting

--- include/biosoup/sequence.hpp
+++ include/biosoup/sequence.hpp
@@ -17,21 +17,27 @@ struct Sequence {
   Sequence(const std::string& name, const std::string& data)
       : Sequence(name.c_str(), name.size(), data.c_str(), data.size()) {}
 
-  Sequence(const char* name, std::uint32_t name_length,
-           const char* data, std::uint32_t data_length)
+  Sequence(
+      const char* name, std::uint32_t name_length,
+      const char* data, std::uint32_t data_length)
       : id(num_objects++),
         name(name, name_length),
         data(data, data_length),
         quality() {}
 
-  Sequence(const std::string& name, const std::string& data,
-           const std::string& quality)
-      : Sequence(name.c_str(), name.size(), data.c_str(), data.size(),
-                 quality.c_str(), quality.size()) {}
+  Sequence(
+      const std::string& name,
+      const std::string& data,
+      const std::string& quality)
+      : Sequence(
+            name.c_str(), name.size(),
+            data.c_str(), data.size(),
+            quality.c_str(), quality.size()) {}
 
-  Sequence(const char* name, std::uint32_t name_length,
-           const char* data, std::uint32_t data_length,
-           const char* quality, std::uint32_t quality_length)
+  Sequence(
+      const char* name, std::uint32_t name_length,
+      const char* data, std::uint32_t data_length,
+      const char* quality, std::uint32_t quality_length)
       : id(num_objects++),
         name(name, name_length),
         data(data, data_length),
