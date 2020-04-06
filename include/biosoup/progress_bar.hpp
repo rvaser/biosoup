555dd4e4906e060016f91b0e9bd279bb7feebdc4
rvaser <robert.vaser@gmail.com>
2020-04-06 11:15:22 +0200

    Change ProgressBar API

--- include/biosoup/progress_bar.hpp
+++ include/biosoup/progress_bar.hpp
@@ -5,17 +5,19 @@
 
 #include <algorithm>
 #include <cstdint>
+#include <iostream>
 #include <string>
 
 namespace biosoup {
 
 class ProgressBar {
  public:
+  // num_ticks element of [1, num_events]
   ProgressBar(std::uint32_t num_events, std::uint32_t num_ticks)
-      : bar_(std::max(std::min(num_ticks, num_events), 1U), '.'),
+      : bar_(std::max(std::min(num_ticks, num_events), 1U), ' '),
         bar_ptr_(0),
         num_events_(num_events),
-        event_id_(0),
+        event_counter_(0),
         events_per_tick_(num_events / static_cast<double>(bar_.size())) {}
 
   ProgressBar(const ProgressBar&) = default;
@@ -26,27 +28,39 @@ class ProgressBar {
 
   ~ProgressBar() = default;
 
-  std::string operator()() {  // return an empty string if bar is unchanged
-    ++event_id_;
-    if (event_id_ >= num_events_) {
+  std::uint32_t event_counter() const {
+    return static_cast<std::uint32_t>(event_counter_);
+  }
+
+  bool operator++() {  // true if a tick is added
+    ++event_counter_;
+    if (event_counter_ > num_events_) {
+      return false;
+    } else if (event_counter_ == num_events_) {
       while (bar_ptr_ < bar_.size()) {
-        bar_[bar_ptr_++] = '#';
+        bar_[bar_ptr_++] = '=';
       }
-      return bar_;
-    } else if (event_id_ >= (bar_ptr_ + 1) * events_per_tick_) {
-      bar_[bar_ptr_++] = '#';
-      return bar_;
-    } else if (event_id_ == 1) {  // possible empty bar
-      return bar_;
+      return true;
+    } else if (event_counter_ >= (bar_ptr_ + 1) * events_per_tick_) {
+      bar_[bar_ptr_++] = '=';
+      if (bar_ptr_ < bar_.size()) {
+        bar_[bar_ptr_] = '>';
+      }
+      return true;
     }
-    return std::string{};
+    return false;
+  }
+
+  // to decrease amount of output, use when ++ returns true
+  friend std::ostream& operator<<(std::ostream& os, const ProgressBar& pb) {
+    return os << pb.bar_;
   }
 
  private:
   std::string bar_;
   std::uint32_t bar_ptr_;
   std::uint32_t num_events_;
-  double event_id_;
+  double event_counter_;
   double events_per_tick_;
 };
 
