e2b39f14307bc36208bc11d4174e3c82b69849cc
rvaser <robert.vaser@gmail.com>
2020-12-17 15:00:09 +0800

    Add quality to NucleicAcid

--- include/biosoup/nucleic_acid.hpp
+++ include/biosoup/nucleic_acid.hpp
@@ -6,6 +6,7 @@
 #include <algorithm>
 #include <atomic>
 #include <cstdint>
+#include <numeric>
 #include <string>
 #include <stdexcept>
 #include <vector>
@@ -52,16 +53,18 @@ class NucleicAcid {
       : id(num_objects++),
         name(name, name_len),
         deflated_data(),
-        inflated_len(data_len) {
-    deflated_data.reserve(inflated_len / 32. + .999);
+        block_quality(),
+        inflated_len(data_len),
+        is_reverse_complement(0) {
+    deflated_data.reserve(data_len / 32. + .999);
     std::uint64_t block = 0;
-    for (std::uint32_t i = 0; i < inflated_len; ++i) {
+    for (std::uint32_t i = 0; i < data_len; ++i) {
       std::uint64_t c = kNucleotideCoder[static_cast<std::uint8_t>(data[i])];
       if (c == 255ULL) {
         throw std::invalid_argument(
             "[biosoup::NucleicAcid::NucleicAcid] error: not a nucleotide");
       }
-      block |= c << (i << 1 & 63);
+      block |= c << ((i << 1) & 63);
       if (((i + 1) & 31) == 0 || i == data_len - 1) {
         deflated_data.emplace_back(block);
         block = 0;
@@ -72,18 +75,32 @@ class NucleicAcid {
   NucleicAcid(
       const std::string& name,
       const std::string& data,
-      const std::string& /* quality */)
+      const std::string& quality)
       : NucleicAcid(
           name.c_str(), name.size(),
-          data.c_str(), data.size()) {}
+          data.c_str(), data.size(),
+          quality.c_str(), quality.size()) {}
 
   NucleicAcid(
       const char* name, std::uint32_t name_len,
       const char* data, std::uint32_t data_len,
-      const char* /* quality */, std::uint32_t /* quality_len */)
+      const char* quality, std::uint32_t quality_len)
       : NucleicAcid(
           name, name_len,
-          data, data_len) {}
+          data, data_len) {
+    block_quality.reserve(quality_len / 64. + .999);
+    for (std::uint32_t i = 0; i < quality_len; i += 64) {
+      std::uint32_t j = std::min(i + 64, quality_len);
+      auto block = std::accumulate(
+          quality + i,
+          quality + j,
+          0,
+          [] (const std::uint32_t& sum, const char& q) -> std::uint32_t {
+            return sum + (q - '!');
+          });
+      block_quality.emplace_back(block / (j - i));
+    }
+  }
 
   NucleicAcid(const NucleicAcid&) = default;
   NucleicAcid& operator=(const NucleicAcid&) = default;
@@ -93,11 +110,23 @@ class NucleicAcid {
 
   ~NucleicAcid() = default;
 
-  std::uint64_t operator[](std::uint32_t i) const {
-    return (deflated_data[i >> 5] >> (i << 1 & 63)) & 3;
+  std::uint64_t Code(std::uint32_t i) const {
+    std::uint64_t x = 0;
+    if (is_reverse_complement) {
+      i = inflated_len - i - 1;
+      x = 3;
+    }
+    return ((deflated_data[i >> 5] >> ((i << 1) & 63)) & 3) ^ x;
   }
 
-  std::string Inflate(std::uint32_t i = 0, std::uint32_t len = -1) const {
+  std::uint8_t Score(std::uint32_t i) const {
+    if (is_reverse_complement) {
+      i = inflated_len - i - 1;
+    }
+    return block_quality[i >> 6];
+  }
+
+  std::string InflateData(std::uint32_t i = 0, std::uint32_t len = -1) const {
     if (i >= inflated_len) {
       return std::string{};
     }
@@ -106,25 +135,27 @@ class NucleicAcid {
     std::string dst{};
     dst.reserve(len);
     for (; len; ++i, --len) {
-      dst += kNucleotideDecoder[operator[](i)];
+      dst += kNucleotideDecoder[Code(i)];
+    }
+    return dst;
+  }
+
+  std::string InflateQuality(std::uint32_t i = 0, std::uint32_t len = -1) const {  // NOLINT
+    if (block_quality.empty() || i >= inflated_len) {
+      return std::string{};
+    }
+    len = std::min(len, inflated_len - i);
+
+    std::string dst{};
+    dst.reserve(len);
+    for (; len; ++i, --len) {
+      dst += Score(i) + '!';
     }
     return dst;
   }
 
   void ReverseAndComplement() {   // Watson-Crick base pairing
-    std::vector<std::uint64_t> tmp;
-    tmp.reserve(deflated_data.size());
-
-    std::uint64_t block = 0;
-    for (std::uint32_t i = 0; i < inflated_len; ++i) {
-      block |= (operator[](inflated_len - i - 1) ^ 3) << (i << 1 & 63);
-      if (((i + 1) & 31) == 0 || i == inflated_len - 1) {
-        tmp.emplace_back(block);
-        block = 0;
-      }
-    }
-
-    deflated_data.swap(tmp);
+    is_reverse_complement ^= 1;
   }
 
   static std::atomic<std::uint32_t> num_objects;
@@ -132,7 +163,9 @@ class NucleicAcid {
   std::uint32_t id;  // (optional) initialize num_objects to 0
   std::string name;
   std::vector<std::uint64_t> deflated_data;
+  std::vector<std::uint8_t> block_quality;  // (optional) Phred quality scores
   std::uint32_t inflated_len;
+  bool is_reverse_complement;
 };
 
 }  // namespace biosoup
